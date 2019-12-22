23abd194ed24a7294dd89c51f6fe0d33db45834b
Taewoo Lee <lteawoo@naver.com>
2019-12-22 20:51:18 +0900

    write 기능 스프링을 이용하지 않으면 입력 값에 대한 예외처리를 if문을 하나하나 써가며 다시 작성하도록 하는 방어적인 코드를 작성해야 한다.

--- src/main/java/kr/taeu/mvc/board/controller/BoardController.java
+++ src/main/java/kr/taeu/mvc/board/controller/BoardController.java
@@ -1,11 +1,13 @@
 package kr.taeu.mvc.board.controller;
 
+import kr.taeu.mvc.board.domain.BoardVO;
 import kr.taeu.mvc.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,5 +25,16 @@ public class BoardController {
     public String read(Model model, @PathVariable int seq) {
         model.addAttribute("boardVO", boardService.read(seq));
         return "/board/read";
+    }
+
+    @RequestMapping(value="/board/write", method=RequestMethod.GET)
+    public String write() {
+        return "/board/write";
+    }
+
+    @RequestMapping(value="/board/write", method=RequestMethod.POST)
+    public String write(BoardVO boardVO) {
+        boardService.write(boardVO);
+        return "redirect:/board/list";
     }
 }
